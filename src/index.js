ab4421829b6dd55d466e113a426d2b23a4dcb69d
kastrubait <60570588+kastrubait@users.noreply.github.com>
2020-03-17 20:05:52 +0300

    add solution task
    add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let num = n.toString();
+    if (num[0] === '-') {
+        num = num.slice(1, num.length);
+    }
+   return  num.split('').reverse().join('');
 }
